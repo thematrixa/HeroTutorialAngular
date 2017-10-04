f1dc5bf0974a79eb8d1cf7635efe36b6c2d643b0
ivegor <ivelin.ivanov@nevexis.com>
2017-10-04 14:43:29 +0300

    dd

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,9 +4,16 @@ import { Hero } from './Hero'
 
 @Injectable()
 export class HeroService {
-  
+
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
 
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
 }
